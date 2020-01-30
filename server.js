c7a9e0a2b4f8322c0a3d2768c9481e01fffe38f0
Antonio <sago.ant@gmail.com>
2020-01-30 19:59:17 +0100

    feat: converted example to users

--- server.js
+++ server.js
@@ -1,113 +1,59 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type RandomDie {
-    numSides: Int!
-    rollOnce: Int!
-    roll(numRolls: Int!): [Int]
-  }
-  
-  type Message {
+const schema = buildSchema(`  
+  type User {
     id: ID!
-    content: String
-    author: String
+    email: String
+    givenName: String
+    familyName: String
+  }
+
+  input UserInput {
+    email: String
+    givenName: String
+    familyName: String
   }
 
   type Query {
-    quoteOfTheDay: String
-    random: Float!
-    getDie(numSides: Int): RandomDie
-    getMessage(id: ID!): Message
-    ip: String
-  }
-
-  input MessageInput {
-    content: String
-    author: String
+    user(id: ID!): User
   }
 
   type Mutation {
-    createMessage(input: MessageInput): Message
-    updateMessage(id: ID!, input: MessageInput): Message
+    createUser(user: UserInput): User
+    updateUser(id: ID!, user: UserInput): User
   }
 `);
 
-class RandomDie {
-  constructor(numSides) {
-    this.numSides = numSides;
-  }
-
-  rollOnce() {
-    return 1 + Math.floor(Math.random() * this.numSides);
-  }
-
-  roll({numRolls}) {
-    var output = [];
-    for (var i = 0; i < numRolls; i++) {
-      output.push(this.rollOnce());
-    }
-    return output;
-  }
-}
-
-// If Message had any complex fields, we'd put them on this object.
-class Message {
-  constructor(id, {content, author}) {
-    this.id = id;
-    this.content = content;
-    this.author = author;
-  }
-}
-
-
-const loggingMiddleware = (req, res, next) => {
-  console.log('ip:', req.ip);
-  next();
-}
-
-var fakeDatabase = {};
+const fakeDatabase = {};
 // The root provides a resolver function for each API endpoint
-var root = {
-  quoteOfTheDay: () => {
-    return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-  },
-  random: () => {
-    return Math.random();
-  },
-  getDie: ({numSides}) => {
-    return new RandomDie(numSides || 6);
-  },
-  getMessage: ({id}) => {
+const root = {
+  user: ({id}) => {
     if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
+      throw new Error(`no users exists with id ${id}`);
     }
-    return new Message(id, fakeDatabase[id]);
+    return fakeDatabase[id];
   },
-  createMessage: ({input}) => {
+  createUser: ({input}) => {
     // Create a random id for our "database".
-    var id = require('crypto').randomBytes(10).toString('hex');
+    const id = require('crypto').randomBytes(10).toString('hex');
 
     fakeDatabase[id] = input;
-    return new Message(id, input);
+    return { id, ...fakeDatabase[id] };
   },
   updateMessage: ({id, input}) => {
     if (!fakeDatabase[id]) {
-      throw new Error('no message exists with id ' + id);
+      throw new Error(`no users exists with id ${id}`);
     }
     // This replaces all old data, but some apps might want partial update.
     fakeDatabase[id] = input;
-    return new Message(id, input);
-  },
-  ip: function (args, request) {
-    return request.ip;
+    return { id, ...fakeDatabase[id] };
   },
 };
 
-var app = express();
-app.use(loggingMiddleware);
+const app = express();
 app.use('/', graphqlHTTP({
   schema: schema,
   rootValue: root,
