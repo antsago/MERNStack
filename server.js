03e04305074634f70ec1e3548d9d2f8de1362399
Antonio <sago.ant@gmail.com>
2020-01-30 18:13:52 +0100

    feat: persistency and mutation

--- server.js
+++ server.js
@@ -9,11 +9,28 @@ var schema = buildSchema(`
     rollOnce: Int!
     roll(numRolls: Int!): [Int]
   }
+  
+  type Message {
+    id: ID!
+    content: String
+    author: String
+  }
 
   type Query {
     quoteOfTheDay: String
     random: Float!
     getDie(numSides: Int): RandomDie
+    getMessage(id: ID!): Message
+  }
+
+  input MessageInput {
+    content: String
+    author: String
+  }
+
+  type Mutation {
+    createMessage(input: MessageInput): Message
+    updateMessage(id: ID!, input: MessageInput): Message
   }
 `);
 
@@ -35,6 +52,16 @@ class RandomDie {
   }
 }
 
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+var fakeDatabase = {};
 // The root provides a resolver function for each API endpoint
 var root = {
   quoteOfTheDay: () => {
@@ -45,7 +72,28 @@ var root = {
   },
   getDie: ({numSides}) => {
     return new RandomDie(numSides || 6);
-  }
+  },
+  getMessage: ({id}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, fakeDatabase[id]);
+  },
+  createMessage: ({input}) => {
+    // Create a random id for our "database".
+    var id = require('crypto').randomBytes(10).toString('hex');
+
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: ({id, input}) => {
+    if (!fakeDatabase[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    fakeDatabase[id] = input;
+    return new Message(id, input);
+  },
 };
 
 var app = express();
