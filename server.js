27410da379583cd802854e0bf7ce1c2eaf97f1a4
Antonio <sago.ant@gmail.com>
2020-01-30 18:02:29 +0100

    feat: adding custom types

--- server.js
+++ server.js
@@ -4,12 +4,36 @@ var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
+  type RandomDie {
+    numSides: Int!
+    rollOnce: Int!
+    roll(numRolls: Int!): [Int]
+  }
+
   type Query {
     quoteOfTheDay: String
     random: Float!
-    rollDice(numDice: Int!, numSides: Int): [Int]
+    getDie(numSides: Int): RandomDie
   }
 `);
+
+class RandomDie {
+  constructor(numSides) {
+    this.numSides = numSides;
+  }
+
+  rollOnce() {
+    return 1 + Math.floor(Math.random() * this.numSides);
+  }
+
+  roll({numRolls}) {
+    var output = [];
+    for (var i = 0; i < numRolls; i++) {
+      output.push(this.rollOnce());
+    }
+    return output;
+  }
+}
 
 // The root provides a resolver function for each API endpoint
 var root = {
@@ -19,12 +43,8 @@ var root = {
   random: () => {
     return Math.random();
   },
-  rollDice: ({numDice, numSides}) => {
-    var output = [];
-    for (var i = 0; i < numDice; i++) {
-      output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-    }
-    return output;
+  getDie: ({numSides}) => {
+    return new RandomDie(numSides || 6);
   }
 };
 
