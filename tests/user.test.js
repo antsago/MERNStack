0263caffb0f162f3ba43eab72c45d6f69b7e51ac
Antonio <sago.ant@gmail.com>
2020-02-04 19:55:35 +0100

    feat: added first real test

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,6 +1,34 @@
+const axios = require('axios');
+const { port } = require('../src/config');
+
+const axiosInstance = axios.create({
+  baseURL: `http://localhost:${port}/`,
+  headers: {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+  }
+});
+
+const makeCall = async (query) => {
+  const response = await axiosInstance.post('/', JSON.stringify({ query }));
+
+  return response.data;
+}
+
 describe('User CRUD', () => {
   test('User can be created', async () => {
-    expect(true).toBeTruthy();
+    const response = await makeCall(`
+      mutation {
+        createUser(user: { email: "foo@test.com" }){id, givenName, email, created}
+      }
+    `);
+
+    expect(response.data.createUser).toEqual(expect.objectContaining({
+      id: expect.any(String),
+      givenName: null,
+      email: 'foo@test.com',
+      created: expect.any(String)
+    }));
   });
 
   test('User can be read', async () => {
