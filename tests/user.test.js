de75fbca0e185180c88bd7208acb873173309f9a
Antonio <sago.ant@gmail.com>
2020-02-04 20:03:44 +0100

    feat: added other e2e tests

--- tests/user.test.js
+++ tests/user.test.js
@@ -32,14 +32,57 @@ describe('User CRUD', () => {
   });
 
   test('User can be read', async () => {
-    expect(true).toBeTruthy();
+    const response = await makeCall(`
+      {
+        user(id: "testing"){id, givenName, email, created}
+      }
+    `);
+
+    expect(response.data.user).toEqual(expect.objectContaining({
+      id: 'testing',
+      givenName: "Test",
+      email: 'test@test.com',
+      created: expect.any(String)
+    }));
   });
 
   test('User can be updated', async () => {
-    expect(true).toBeTruthy();
+    const response = await makeCall(`
+      mutation {
+        updateUser(id:"testing", user: { givenName: "Changed" }){id, givenName, email, created}
+      }
+    `);
+
+    expect(response.data.updateUser).toEqual(expect.objectContaining({
+      id: 'testing',
+      givenName: "Changed",
+      email: 'test@test.com',
+      created: expect.any(String)
+    }));
   });
 
   test('User can be deleted', async () => {
-    expect(true).toBeTruthy();
+    const response = await makeCall(`
+      mutation {
+        deleteUser(id:"testing"){id, givenName, email, created}
+      }
+    `);
+
+    expect(response.data.deleteUser).toEqual(expect.objectContaining({
+      id: 'testing',
+      givenName: "Changed",
+      email: 'test@test.com',
+      created: expect.any(String)
+    }));
+  });
+
+  test('Deleted user is not found', async () => {
+    const response = await makeCall(`
+      {
+        user(id: "testing"){id}
+      }
+    `);
+
+    expect(response.data.user).toEqual(null);
   });
 });
