236568fe83726af4e16accfaa5c1a07b32ba1e76
Antonio <sago.ant@gmail.com>
2020-02-06 13:15:20 +0100

    feat: next apis

--- client/pages/about.js
+++ client/pages/about.js
@@ -1,9 +1,54 @@
+import useSWR from 'swr';
+import axios from 'axios';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 
+async function fetcher(url) {
+  const res = await axios.get(url);
+  return res.data;
+}
+
 export default function Index() {
+  const { query } = useRouter();
+  const { data, error } = useSWR(
+    `/api/randomQuote${query.author ? '?author=' + query.author : ''}`,
+    fetcher
+  );
+  // The following line has optional chaining, added in Next.js v9.1.5,
+  // is the same as `data && data.author`
+  const author = data?.author;
+  let quote = data?.quote;
+
+  if (!data) quote = 'Loading...';
+  if (error) quote = 'Failed to fetch the quote.';
+
   return (
     <Layout>
-      <p>This is the about page</p>
+      <main className="center">
+        <div className="quote">{quote}</div>
+        {author && <span className="author">- {author}</span>}
+      </main>
+
+      <style jsx>{`
+        main {
+          width: 90%;
+          max-width: 900px;
+          margin: 300px auto;
+          text-align: center;
+        }
+        .quote {
+          font-family: cursive;
+          color: #e243de;
+          font-size: 24px;
+          padding-bottom: 10px;
+        }
+        .author {
+          font-family: sans-serif;
+          color: #559834;
+          font-size: 20px;
+        }
+      `}</style>
     </Layout>
   );
 }
+
