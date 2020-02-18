1b6ca2e38fa9b8a2833f8a08a8d52dbe6c86dd44
Antonio <sago.ant@gmail.com>
2020-02-18 18:39:25 +0100

    refactor: extracted Footer

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -12,7 +12,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import Copyright from '../components/Copyright';
+import Footer from '../components/Footer';
 
 const useStyles = makeStyles(theme => ({
   icon: {
@@ -39,10 +39,6 @@ const useStyles = makeStyles(theme => ({
   },
   cardContent: {
     flexGrow: 1,
-  },
-  footer: {
-    backgroundColor: theme.palette.background.paper,
-    padding: theme.spacing(6),
   },
 }));
 
@@ -123,17 +119,8 @@ export default function Album() {
           </Grid>
         </Container>
       </main>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Typography variant="h6" align="center" gutterBottom>
-          Footer
-        </Typography>
-        <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
-        </Typography>
-        <Copyright />
-      </footer>
-      {/* End footer */}
+
+      <Footer />
     </React.Fragment>
   );
 }
