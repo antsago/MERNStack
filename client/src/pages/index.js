2a53012a006e8025c045d4d9a3332709f05d3faa
Antonio <sago.ant@gmail.com>
2020-02-18 19:27:02 +0100

    refactor: changed exporting style of index

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,19 +4,19 @@ import { Layout, UserItem } from '../components';
 
 const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-export default function Album() {
-  return (
-    <Layout>
-      <Grid container spacing={4}>
-        {cards.map(card => (
-          <UserItem
-            key={card}
-            image="https://source.unsplash.com/random"
-            heading="Heading"
-            content="This is a media card. You can use this section to describe the content."
-          />
-        ))}
-      </Grid>
-    </Layout>
-  );
-}
+const Index = () => (
+  <Layout>
+    <Grid container spacing={4}>
+      {cards.map(card => (
+        <UserItem
+          key={card}
+          image="https://source.unsplash.com/random"
+          heading="Heading"
+          content="This is a media card. You can use this section to describe the content."
+        />
+      ))}
+    </Grid>
+  </Layout>
+);
+
+export default Index;
