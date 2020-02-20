8d076be70466ab6edee794adb955072ebfef801b
Antonio <sago.ant@gmail.com>
2020-02-20 18:31:38 +0100

    fix: centered loaders

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -2,8 +2,15 @@ import React from 'react';
 import { gql } from 'apollo-boost';
 import { useQuery } from '@apollo/react-hooks';
 import withApollo from '../withApollo';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { makeStyles, Grid, CircularProgress } from '@material-ui/core';
 import { Layout, UserItem } from '../components';
+
+const useStyles = makeStyles(theme => ({
+  loader: {
+    position: 'relative',
+    left: '50%',
+  },
+}));
 
 const QUERY = gql`
   {
@@ -13,10 +20,12 @@ const QUERY = gql`
 
 const Index = () => {
   const { data, loading } = useQuery(QUERY);
+  const classes = useStyles();
 
   return (
     <Layout>
-      {loading || !data
+      <CircularProgress className={classes.loader}/>
+      {/* {loading || !data
         ? <CircularProgress />
         : <Grid container spacing={4}>
           {data.users.map(user => (
@@ -28,7 +37,7 @@ const Index = () => {
             />
           ))}
         </Grid>
-      }
+      } */}
     </Layout>
   );
 }
