2b3013de68cd8b93927dfeb88f0b9786599c5cf5
Antonio <sago.ant@gmail.com>
2020-03-09 12:50:08 +0100

    refactor: removed unused import

--- client/src/common/layout/Copyright.tsx
+++ client/src/common/layout/Copyright.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Typography, Link } from '@material-ui/core'
+import { Typography } from '@material-ui/core'
 
 const Copyright = () => (
   <Typography variant='body2' color='textSecondary' align='center'>
