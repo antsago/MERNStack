f4061051f77233b0de573076a8c6c9fac043d8d2
Antonio <sago.ant@gmail.com>
2020-03-11 12:37:34 +0100

    feat: added PWA

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -3,8 +3,11 @@ import BaseDocument, { Html, Head, Main, NextScript } from 'next/document'
 import { ServerStyleSheets } from '@material-ui/core/styles'
 import { theme } from '../common'
 
+const APP_NAME = 'Users List'
+const APP_DESCRIPTION = 'An example app to explore the MERN stack'
+
 export default class Document extends BaseDocument {
-  static async getInitialProps (ctx) {
+  static async getInitialProps(ctx) {
     const sheets = new ServerStyleSheets()
     const originalRenderPage = ctx.renderPage
 
@@ -23,15 +26,24 @@ export default class Document extends BaseDocument {
     }
   }
 
-  render () {
+  render() {
     return (
-      <Html>
+      <Html lang='en' dir='ltr'>
         <Head>
-          <meta
-            name='viewport'
-            content='minimum-scale=1, initial-scale=1, width=device-width'
-          />
+          <meta name='application-name' content={APP_NAME} />
+          <meta name='apple-mobile-web-app-capable' content='yes' />
+          <meta name='apple-mobile-web-app-status-bar-style' content='default' />
+          <meta name='apple-mobile-web-app-title' content={APP_NAME} />
+          <meta name='description' content={APP_DESCRIPTION} />
+          <meta name='format-detection' content='telephone=no' />
+          <meta name='mobile-web-app-capable' content='yes' />
           <meta name='theme-color' content={theme.palette.primary.main} />
+          <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
+
+          <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+          <link rel='manifest' href='/manifest.json' />
+          <link rel='shortcut icon' href='/icons/favicon.ico' />
+
           <link
             rel='stylesheet'
             href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
