4dd592c1a48278143386765beb2a88ecae68294e
Antonio <sago.ant@gmail.com>
2020-03-13 11:33:57 +0100

    feat: added icons to toolbar

--- client/src/common/layout/Header.tsx
+++ client/src/common/layout/Header.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-import { Group as UsersIcon } from '@material-ui/icons'
-import { AppBar, Toolbar, Typography, makeStyles } from '@material-ui/core'
+import { Group as UsersIcon, Add, PlusOne } from '@material-ui/icons'
+import { AppBar, Toolbar, Typography, makeStyles, createStyles, IconButton } from '@material-ui/core'
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(theme => createStyles({
   icon: {
     marginRight: theme.spacing(2)
+  },
+  grow: {
+    flexGrow: 1,
   }
 }))
 
@@ -16,8 +19,11 @@ const Header = () => {
       <Toolbar>
         <UsersIcon className={classes.icon} />
         <Typography variant='h6' color='inherit' noWrap>
-          Users
+          UsersList
         </Typography>
+        <div className={classes.grow} />
+        <IconButton color="inherit"><Add /></IconButton>
+        <IconButton color="inherit"><PlusOne /></IconButton>
       </Toolbar>
     </AppBar>
   )
