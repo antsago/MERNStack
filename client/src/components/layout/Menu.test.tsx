fc3913378fa4b574ff98b14518819bf4b17b7c1a
Antonio <sago.ant@gmail.com>
2020-03-13 12:48:19 +0100

    feat: added create functionality to menu

--- client/src/components/layout/Menu.test.tsx
+++ client/src/components/layout/Menu.test.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import Menu from './Menu'
+import { render, fireEvent } from '@testing-library/react'
+import { Menu } from './Menu'
 
 describe('Menu', () => {
   test('Renders correctly', () => {
-    const { queryAllByRole } = render(<Menu />)
-    const buttons = queryAllByRole('button')
-    expect(buttons[0]).toBeInTheDocument()
-    expect(buttons[1]).toBeInTheDocument()
+    const { getByLabelText } = render(<Menu createUser={() => { }} createRandomUser={() => { }} />)
+    expect(getByLabelText('Add user')).toBeInTheDocument()
+    expect(getByLabelText('Add random user')).toBeInTheDocument()
+  })
+
+  test('Call random user on clicking', () => {
+    const createRandomUser = jest.fn()
+    const { getByLabelText } = render(<Menu createUser={() => { }} createRandomUser={createRandomUser} />)
+
+    fireEvent.click(getByLabelText('Add random user'))
+    expect(createRandomUser).toHaveBeenCalled()
+  })
+
+  test('Add user flow', () => {
+    const createUser = jest.fn()
+    const { getByLabelText, queryByRole, getByText } = render(<Menu createRandomUser={() => { }} createUser={createUser} />)
+
+    fireEvent.click(getByLabelText('Add user'))
+    expect(queryByRole('dialog')).toBeInTheDocument()
+    fireEvent.click(getByText('Save'))
+    expect(queryByRole('dialog')).not.toBeInTheDocument()
+    expect(createUser).toHaveBeenCalled()
   })
 })
--- client/src/components/layout/Menu.tsx
+++ client/src/components/layout/Menu.tsx
@@ -1,14 +1,38 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState } from 'react'
+import { connect } from 'react-redux'
 import { Add, PlusOne } from '@material-ui/icons'
 import { IconButton } from '@material-ui/core'
+import { UserDialog } from '../users'
+import { UserInput } from '../../utils'
 
-const Menu = () => {
+export const Menu = ({ createUser, createRandomUser }: { createUser: (UserInput) => void, createRandomUser: () => void }) => {
+  const [showDialog, setShowDialog] = useState(false)
   return (
     <Fragment>
-      <IconButton color="inherit"><Add /></IconButton>
-      <IconButton color="inherit"><PlusOne /></IconButton>
+      <IconButton color="inherit" onClick={() => setShowDialog(true)} aria-label="Add user">
+        <Add />
+      </IconButton>
+      <IconButton color="inherit" edge="end" onClick={createRandomUser} aria-label="Add random user">
+        <PlusOne />
+      </IconButton>
+      {showDialog && (
+        <UserDialog
+          user={{}}
+          onClose={() => setShowDialog(false)}
+          onSubmit={newUser => {
+            createUser(newUser)
+            setShowDialog(false)
+          }}
+          open
+        />
+      )}
     </Fragment>
   )
 }
 
-export default Menu
+const mapDispatchToProps = dispatch => ({
+  createUser: () => { },
+  createRandomUser: () => { }
+})
+
+export default connect(undefined, mapDispatchToProps)(Menu)
