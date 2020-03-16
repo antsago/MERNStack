f7e790af7271327c6521e0ea927d482fec377f60
Antonio <sago.ant@gmail.com>
2020-03-16 13:10:46 +0100

    feat: added saga CRUD

--- client/src/utils/state/usersDuck/saga.test.ts
+++ client/src/utils/state/usersDuck/saga.test.ts
@@ -1,34 +1,132 @@
 import { expectSaga } from 'redux-saga-test-plan'
 import ApiClient from '../../ApiClient'
 import { ManageAlerts } from '../utilsDuck'
-import { loadUsers as loadUsersSaga } from './saga'
-import { loadUsers, loadUsersSuccess } from './actions'
+import usersSaga from './saga'
+import {
+  createUser,
+  loadUsers,
+  updateUser,
+  deleteUser,
+  createUserSuccess,
+  loadUsersSuccess,
+  updateUserSuccess,
+  deleteUserSuccess
+} from './actions'
 
 describe('Users Saga', () => {
-  test('Users saga finds and loads the users', async () => {
-    const users = [{ id: 'test', email: 'test@test.com' }]
-    const mockedClient = { makeQuery: jest.fn().mockResolvedValue({ users }) }
+  test('Create user creates new user', async () => {
+    const newUser = { email: 'test@test.com' }
+    const user = { id: 'test', ...newUser }
+    const mockedClient = { createUser: jest.fn().mockResolvedValue(user) }
 
-    await expectSaga(loadUsersSaga, (mockedClient as any) as ApiClient)
-      .put(loadUsersSuccess(users))
-      .dispatch(loadUsers())
-      .run()
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put(createUserSuccess(user))
+      .dispatch(createUser(newUser))
+      .silentRun()
 
-    expect(mockedClient.makeQuery).toHaveBeenCalled()
+    expect(mockedClient.createUser).toHaveBeenCalled()
   })
 
-  test('Users saga handles error', async () => {
+  test('Create user handles error', async () => {
+    const newUser = { email: 'test@test.com' }
     const message = 'testing!'
     const error = new Error(message)
-    const mockedClient = { makeQuery: jest.fn().mockRejectedValue(error) }
+    const mockedClient = { createUser: jest.fn().mockRejectedValue(error) }
 
-    await expectSaga(loadUsersSaga, (mockedClient as any) as ApiClient)
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put.like({
+        action: { type: ManageAlerts.ADD_ALERT, alert: { message } }
+      })
+      .dispatch(createUser(newUser))
+      .silentRun()
+
+    expect(mockedClient.createUser).toHaveBeenCalled()
+  })
+
+  test('Load users finds and loads the users', async () => {
+    const users = [{ id: 'test', email: 'test@test.com' }]
+    const mockedClient = { loadUsers: jest.fn().mockResolvedValue(users) }
+
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put(loadUsersSuccess(users))
+      .dispatch(loadUsers())
+      .silentRun()
+
+    expect(mockedClient.loadUsers).toHaveBeenCalled()
+  })
+
+  test('Load users handles error', async () => {
+    const message = 'testing!'
+    const error = new Error(message)
+    const mockedClient = { loadUsers: jest.fn().mockRejectedValue(error) }
+
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
       .put.like({
         action: { type: ManageAlerts.ADD_ALERT, alert: { message } }
       })
       .dispatch(loadUsers())
-      .run()
+      .silentRun()
 
-    expect(mockedClient.makeQuery).toHaveBeenCalled()
+    expect(mockedClient.loadUsers).toHaveBeenCalled()
+  })
+
+  test('Update user changes user', async () => {
+    const newUser = { email: 'test@test.com' }
+    const id = 'test'
+    const user = { id, ...newUser }
+    const mockedClient = { updateUser: jest.fn().mockResolvedValue(user) }
+
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put(updateUserSuccess(user))
+      .dispatch(updateUser(id, newUser))
+      .silentRun()
+
+    expect(mockedClient.updateUser).toHaveBeenCalled()
+  })
+
+  test('Update user handles error', async () => {
+    const newUser = { email: 'test@test.com' }
+    const id = 'test'
+    const message = 'testing!'
+    const error = new Error(message)
+    const mockedClient = { updateUser: jest.fn().mockRejectedValue(error) }
+
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put.like({
+        action: { type: ManageAlerts.ADD_ALERT, alert: { message } }
+      })
+      .dispatch(updateUser(id, newUser))
+      .silentRun()
+
+    expect(mockedClient.updateUser).toHaveBeenCalled()
+  })
+
+  test('Delete user deletes user', async () => {
+    const id = 'test'
+    const user = { id, email: 'test@test.com' }
+    const mockedClient = { deleteUser: jest.fn().mockResolvedValue(user) }
+
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put(deleteUserSuccess(id))
+      .dispatch(deleteUser(id))
+      .silentRun()
+
+    expect(mockedClient.deleteUser).toHaveBeenCalled()
+  })
+
+  test('Delete user handles error', async () => {
+    const id = 'test'
+    const message = 'testing!'
+    const error = new Error(message)
+    const mockedClient = { deleteUser: jest.fn().mockRejectedValue(error) }
+
+    await expectSaga(usersSaga, (mockedClient as any) as ApiClient)
+      .put.like({
+        action: { type: ManageAlerts.ADD_ALERT, alert: { message } }
+      })
+      .dispatch(deleteUser(id))
+      .silentRun()
+
+    expect(mockedClient.deleteUser).toHaveBeenCalled()
   })
 })
--- client/src/utils/state/usersDuck/saga.ts
+++ client/src/utils/state/usersDuck/saga.ts
@@ -1,21 +1,60 @@
-import { put, takeEvery } from 'redux-saga/effects'
+import { put, takeEvery, all } from 'redux-saga/effects'
+import { UserInput } from '../../types'
 import ApiClient from '../../ApiClient'
 import { addAlert } from '../utilsDuck'
 import { Actions } from './types'
-import { loadUsersSuccess } from './actions'
+import {
+  createUserSuccess,
+  loadUsersSuccess,
+  updateUserSuccess,
+  deleteUserSuccess
+} from './actions'
 
-export function * loadUsers (apiClient: ApiClient) {
+function * createUser (apiClient: ApiClient, action) {
   try {
-    const response = yield apiClient.makeQuery(
-      '{ users{id, givenName, familyName, email, created} }'
-    )
-    yield put(loadUsersSuccess(response.users))
+    const user = yield apiClient.createUser(action.user)
+    yield put(createUserSuccess(user))
+  } catch (err) {
+    const message = err.message || 'We found an error while creating users'
+    yield put(addAlert(message))
+  }
+}
+
+function * loadUsers (apiClient: ApiClient) {
+  try {
+    const users = yield apiClient.loadUsers()
+    yield put(loadUsersSuccess(users))
   } catch (err) {
     const message = err.message || 'We found an error while loading users'
     yield put(addAlert(message))
   }
 }
 
-export default function * saga () {
-  yield takeEvery(Actions.LOAD_USERS, loadUsers, new ApiClient())
+function * updateUser (apiClient: ApiClient, action) {
+  try {
+    const changedUser = yield apiClient.updateUser(action.id, action.user)
+    yield put(updateUserSuccess(changedUser))
+  } catch (err) {
+    const message = err.message || 'We found an error while updating users'
+    yield put(addAlert(message))
+  }
+}
+
+function * deleteUser (apiClient: ApiClient, action) {
+  try {
+    const user = yield apiClient.deleteUser(action.id)
+    yield put(deleteUserSuccess(user.id))
+  } catch (err) {
+    const message = err.message || 'We found an error while deleting users'
+    yield put(addAlert(message))
+  }
+}
+
+export default function * saga (apiClient: ApiClient = new ApiClient()) {
+  yield all([
+    takeEvery(Actions.CREATE_USER, createUser, apiClient),
+    takeEvery(Actions.LOAD_USERS, loadUsers, apiClient),
+    takeEvery(Actions.UPDATE_USER, updateUser, apiClient),
+    takeEvery(Actions.DELETE_USER, deleteUser, apiClient)
+  ])
 }
