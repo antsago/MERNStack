7c0d629286ff6e557b9fed5ceea8e0435432ef47
Antonio <sago.ant@gmail.com>
2020-04-14 12:18:55 +0200

    feat: update cache after createUser

--- packages/client/src/utils/ApolloClient/CreateUser.ts
+++ packages/client/src/utils/ApolloClient/CreateUser.ts
@@ -1,6 +1,7 @@
 import { UserInput } from "@mernstack/shared"
 import { gql } from "apollo-boost"
 import { useMutation } from "@apollo/react-hooks"
+import { GET_USERS } from "./GetUsers"
 
 export const CREATE_USER = gql`
   mutation createUser($user: UserInput!) {
@@ -16,5 +17,16 @@ export const CREATE_USER = gql`
 
 export default () => {
   const [createUser] = useMutation(CREATE_USER)
-  return (user: UserInput) => createUser({ variables: { user } })
+  return (user: UserInput) =>
+    createUser({
+      variables: { user },
+      update: (cache, { data: { createUser: newUser } }) => {
+        const { users } = cache.readQuery({ query: GET_USERS })
+
+        cache.writeQuery({
+          query: GET_USERS,
+          data: { users: [...users, newUser] },
+        })
+      },
+    })
 }
