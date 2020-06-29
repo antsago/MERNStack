1465ea60581ef96c2e91f55dd06c229416555455
Antonio <sago.ant@gmail.com>
2020-06-29 16:55:01 +0200

    feat: some server side rendering

--- packages/test4/src/page/index.tsx
+++ packages/test4/src/page/index.tsx
@@ -3,9 +3,6 @@ import * as ReactDOM from "react-dom";
 import App from './App';
 
 const mountNode = document.getElementById("app");
-const app = <App name="Me" />
-if (process.env.NODE_ENV === "development") {
-  ReactDOM.render(app, mountNode);
-} else {
-  ReactDOM.hydrate(app, mountNode)
-}
+
+const app = <App name="Hidrated" />
+ReactDOM.hydrate(app, mountNode)
---
+++ packages/test4/src/server/server.tsx
@@ -0,0 +1,38 @@
+import express from "express"
+import fs from 'fs'
+import path from 'path'
+import React from 'react'
+import ReactDOMServer from 'react-dom/server'
+import App from '../page/App'
+
+function main() {
+  const app = express();
+  const port = 8080;
+
+  const pageDirectory = process.env.NODE_ENV === "development" ?
+    path.resolve(__dirname, '..', '..', 'dist', 'page')
+    : path.resolve(__dirname, '..', '..', 'page')
+  app.use("/dist/page", express.static(pageDirectory));
+
+  app.get("/*", (req, res) => {
+    const renderedApp = ReactDOMServer.renderToString(<App name="server" />);
+    const indexFile = path.resolve(pageDirectory, 'index.html')
+
+    const htmlTemplate = fs.readFileSync(indexFile, 'utf8')
+  
+    return res.send(
+      htmlTemplate.replace('<div id="app"></div>', `<div id="app">${renderedApp}</div>`)
+    )
+  })
+
+  const server = app.listen(port, () => {
+    console.log(`Server listening on port ${port}`)
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => server.close());
+  }
+}
+
+main();
