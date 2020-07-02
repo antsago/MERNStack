c9c47079fac128e83d04ca272b1a7c5a0a5cf2ba
Antonio <sago.ant@gmail.com>
2020-07-02 13:31:02 +0200

    refactor: extracted static assets function

--- packages/test4/src/server/server.tsx
+++ packages/test4/src/server/server.tsx
@@ -4,32 +4,33 @@ import path from 'path'
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 import App from '../page/App'
-import webpack from "webpack"
+
+async function getStaticAssets(isDevelopment, distDirectory) {
+  if (isDevelopment) {
+    const webpack = (await import('webpack')).default
+    const middleware = (await import('webpack-dev-middleware')).default
+    const webpackConfig = (await import('../../webpack.page.config.js')).default
+
+    const compiler = webpack(webpackConfig)
+    return middleware(compiler, { publicPath: '/' })
+  } else {
+    return express.static(distDirectory)
+  }
+}
 
 async function main() {
   const app = express();
   const port = 8080;
 
   const isDevelopment = process.env.NODE_ENV === "development"
-  const pageDirectory = isDevelopment ? path.resolve(__dirname, '..', '..', 'dist', 'page') : path.resolve(__dirname, '..', 'page')
+  const distDirectory = isDevelopment ? path.resolve(__dirname, '..', '..', 'dist', 'page') : path.resolve(__dirname, '..', 'page')
   
-  if (process.env.NODE_ENV === "development") {
-    const webpack = (await import('webpack')).default
-    const middleware = (await import('webpack-dev-middleware')).default
-    const webpackConfig = (await import('../../webpack.page.config.js')).default
-
-    const compiler = webpack(webpackConfig)
-    app.use(middleware(compiler, {
-      publicPath: webpackConfig.output.publicPath
-    }))
-  } else {
-    // the prefix should be the same as webpackConfig.output.publicPath
-    app.use("/static", express.static(pageDirectory));
-  }
+  // the prefix should be the same as webpackConfig.output.publicPath
+  app.use("/static", await getStaticAssets(isDevelopment, distDirectory))
   
   app.get("/*", (req, res) => {
     const renderedApp = ReactDOMServer.renderToString(<App name="server" />);
-    const indexFile = path.resolve(pageDirectory, 'index.html')
+    const indexFile = path.resolve(distDirectory, 'index.html')
 
     const htmlTemplate = fs.readFileSync(indexFile, 'utf8')
   
