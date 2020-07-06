1b32659122b915eec95d40903788663b9535e0e9
Antonio <sago.ant@gmail.com>
2020-07-06 20:53:59 +0200

    fix: improved bundle size

--- packages/test4/src/page/App.tsx
+++ packages/test4/src/page/App.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
-import { CssBaseline, ThemeProvider} from "@material-ui/core"
+import CssBaseline from "@material-ui/core/CssBaseline"
+import { ThemeProvider } from "@material-ui/core/styles"
 import { ApolloClient } from 'apollo-client'
 import { NormalizedCacheObject } from "apollo-cache-inmemory"
 import { ApolloProvider } from '@apollo/react-hooks'
