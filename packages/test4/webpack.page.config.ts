689dd1b3d95782ca5b2d72e23e12916b68b33a3f
Antonio <sago.ant@gmail.com>
2020-07-06 18:43:01 +0200

    feat: split webpack.page.config into dev and prod

--- packages/test4/webpack.page.config.ts
+++ packages/test4/webpack.page.config.ts
@@ -3,44 +3,59 @@ import HtmlWebpackPlugin from 'html-webpack-plugin'
 import HtmlWebpackHarddiskPlugin from 'html-webpack-harddisk-plugin'
 import { GenerateSW } from 'workbox-webpack-plugin'
 
-const config = ({ isProd }: Record<'isProd', boolean>): Configuration => ({
-  name: "page",
-  mode: isProd ? "production" : "development",
-  target: 'web',
-  entry: {
-    page: './src/page/index.tsx',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: ['babel-loader', 'ts-loader'],
-        include: `${__dirname}/src/page`,
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-    modules: ["src", "node_modules"],
-  },
-  output: {
-    path: `${__dirname}/dist/page`,
-    filename: '[name].[hash].js',
-    publicPath: '/assets',
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/page/index.html',
-      favicon: './src/page/favicon.ico',
-      alwaysWriteToDisk: true, // added by HtmlWebpackHarddiskPlugin
-    }),
-    new HtmlWebpackHarddiskPlugin(),
-    new GenerateSW({
-      clientsClaim: true,
-      skipWaiting: true,
-      swDest: "serviceWorker.js",
-    }),
-  ],
-})
+const config = ({ isProd }: Record<'isProd', boolean>): Configuration => {
+  const base: Configuration = {
+    name: "page",
+    target: 'web',
+    entry: {
+      page: './src/page/index.tsx',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: ['babel-loader', 'ts-loader'],
+          include: `${__dirname}/src/page`,
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+      modules: ["src", "node_modules"],
+    },
+    output: {
+      path: `${__dirname}/dist/page`,
+      filename: '[name].[hash].js',
+      publicPath: '/assets',
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/page/index.html',
+        favicon: './src/page/favicon.ico',
+        alwaysWriteToDisk: true, // added by HtmlWebpackHarddiskPlugin
+      }),
+      new HtmlWebpackHarddiskPlugin(),
+      new GenerateSW({
+        clientsClaim: true,
+        skipWaiting: true,
+        swDest: "serviceWorker.js",
+      }),
+    ],
+  }
+
+  const prodConfig: Configuration = {
+    ...base,
+    mode: "production",
+    devtool: 'source-map',
+  }
+
+  const devConfig: Configuration = {
+    ...base,
+    mode: "development",
+    devtool: 'inline-source-map',
+  }
+
+  return isProd ? prodConfig : devConfig
+}
 
 export default config
