28c32f05f9aea8006884dafa2dad8aa4e74acd3a
Antonio <sago.ant@gmail.com>
2020-07-09 02:11:07 +0200

    refactor: removed now obsolete tests

--- packages/test/app/pages/Error.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Page from '../components/Page.jsx'
-const Error = () => (
-  <Page>
-    Error!
-  </Page>
-)
-export default Error
--- packages/test/app/pages/Loading.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Page from '../components/Page.jsx'
-const Loading = () => (
-  <Page>
-    Loading...
-  </Page>
-)
-export default Loading
